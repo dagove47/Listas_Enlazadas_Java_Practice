8224fa7fa3db19d47c9a649786aa8a626ffab74e
David Gomez <dagove47@gmail.com>
2022-11-10 23:27:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Listas_Enlazadas_Java_Practice
+ David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
---
+++ src/main/java/Listas_Enlazadas/Listas_Enlazadas_Java_Practice.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Listas_Enlazadas;
+
+/**
+ *
+ * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
+ */
+public class Listas_Enlazadas_Java_Practice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
